3339b0267aa10a93300fc0776294d50cef730bc8
Colby Garland <colbygarland@gmail.com>
2020-06-15 23:14:00 -0600

    Login page updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,85 +1,90 @@
-
 export default {
-  mode: 'spa',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/auth',
-    'vue-sweetalert2/nuxt',
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-    baseURL: 'http://127.0.0.1:8000/api',
-    headers: {
-      common: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      }
-    }
-  },
-
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: { url: 'login', method: 'post', propertyName: 'data.token' },
-          user: { url: 'me', method: 'get', propertyName: 'data' },
-          logout: false
-        }
-      }
-    }
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
+    mode: 'spa',
     /*
-    ** You can extend webpack config here
+    ** Headers of the page
     */
-    extend (config, ctx) {
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap'}
+        ]
+    },
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: {color: '#fff'},
+    /*
+    ** Global CSS
+    */
+    css: [],
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [],
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+        '@nuxtjs/tailwindcss',
+    ],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        '@nuxtjs/auth',
+        'vue-sweetalert2/nuxt',
+    ],
+    /*
+    ** Axios module configuration
+    ** See https://axios.nuxtjs.org/options
+    */
+    axios: {
+        baseURL: 'http://127.0.0.1:8000/api',
+        headers: {
+            common: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+            }
+        }
+    },
+
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: { url: '/login', method: 'post', propertyName: 'crsf_token' },
+                    logout: false,
+                    user : false,
+                },
+                // tokenRequired: true,
+                // tokenType: 'bearer',
+                // globalToken: true,
+                // autoFetchUser: true
+            }
+        }
+    },
+
+    router: {
+        middleware: ['auth']
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+        }
     }
-  }
 }
--- pages/login.vue
+++ pages/login.vue
@@ -1,49 +1,56 @@
 <template>
-  <div class="">
-      <div class="p-12 shadow rounded max-w-lg bg-white">
-        <h1 class="text-4xl text-center">Login to Hourglass</h1>
-        <form @submit.prevent="login">
-          <div class="p-2">
-            <label class="block pb-1">Email</label>
-            <input v-model="email" type="email" name="email" required autofocus placeholder="Enter email address" class="border-2 p-1 w-full" />
-          </div>
-          <div class="p-2">
-            <label class="block pb-1">Password</label>
-            <input v-model="password" type="password" name="password" required autofocus class="border-2 p-1 w-full" />
-          </div>
-          <div class="p-2">
-            <button type="submit" class="px-5 py-2 bg-secondary text-white">Login</button>
-          </div>
-        </form>
+    <div>
+        <div class="flex bg-gray-100 min-h-screen min-w-full">
+            <div class="m-auto pl-3 pr-3 md:w-1/2 sm:w-auto md:w-1/2 lg:w-1/3 lg:pl-12 lg:pr-12">
+                <div class="max-w-sm m-auto block">
+                    <h1 class="text-4xl font-bold mb-2">Login to Hourglass</h1>
+                    <form @submit.prevent="userLogin">
+                        <div class="py-2">
+                            <label class="block pb-1 font-bold">Email</label>
+                            <input v-model="login.email" type="email" name="email" required autofocus
+                                   placeholder="Enter email address" class="border-2 p-1 w-full"/>
+                        </div>
+                        <div class="py-2">
+                            <label class="block pb-1 font-bold">Password</label>
+                            <input v-model="login.password" type="password" name="password" required autofocus
+                                   class="border-2 p-1 w-full"/>
+                        </div>
+                        <div class="py-6">
+                            <button type="submit" class="px-8 py-2 bg-teal-500 text-white hover:bg-teal-600 font-bold rounded-lg">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="hidden md:block md:w-1/2 lg:w-2/3">
+                <img src="~/assets/img/leaves.jpg" alt="Leaves" class="max-h-screen w-full object-cover"  />
+            </div>
+        </div>
 
-      </div>
-  </div>
+    </div>
 </template>
 
 <script>
-  import swal from 'sweetalert2';
-
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-      }
-    },
-    layout: 'unauth',
-    methods: {
-      login(){
-        this.$axios.post('/login', {
-          email: this.email,
-          password: this.password
-        }).then(function (response) {
-          console.log(response.data);
-          localStorage.setItem('token', response.data.csrf_token);
-        }).catch(function (error) {
-          console.log(error);
-          localStorage.removeItem('token');
-        });
-      }
+    export default {
+        data() {
+            return {
+                login: {
+                    email: '',
+                    password: '',
+                }
+            }
+        },
+        layout: 'default',
+        methods: {
+            async userLogin() {
+                try {
+                    let response = await this.$auth.loginWith('local', {data: this.login});
+                    console.log(response);
+                    this.$router.push('/');
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        },
+        auth: false,
     }
-  }
 </script>
